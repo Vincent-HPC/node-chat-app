cac47b321ea61ef86bc3d0730add598f57362b4b
PCH <pchuang92738@gmail.com>
2018-02-26 10:34:14 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -7,28 +7,28 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    // call emit right here inside of connect callback func.
-    // cuz don't want emit the event until we are connected socket.
-    // Can pass any data in as the second argument.
-    socket.emit('createEmail', {
-        to: 'jen@example.com',
-        text: 'Hey. This is Andrew.'
-    });
-
-    // When emmiting a custom events, 1st arg. is the event name, 2nd is the data
-    socket.emit('createMessage', {
-        from: 'jen',
-        text: 'This message is sent from cliet'
-    });
+    // // call emit right here inside of connect callback func.
+    // // cuz don't want emit the event until we are connected socket.
+    // // Can pass any data in as the second argument.
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey. This is Andrew.'
+    // });
+    //
+    // // When emmiting a custom events, 1st arg. is the event name, 2nd is the data
+    // socket.emit('createMessage', {
+    //     from: 'jen',
+    //     text: 'This message is sent from cliet'
+    // });
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function(email) {
-    console.log('New email', email);
-});
+// socket.on('newEmail', function(email) {
+//     console.log('New email', email);
+// });
 
 socket.on('newMessage', function(message) {
     console.log('New message', message);
--- server/server.js
+++ server/server.js
@@ -16,26 +16,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // this is not a listener so not goning to provide a callback func.
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey. What is going on.',
-        createAt: 123
-    });
+    // // this is not a listener so not goning to provide a callback func.
+    // socket.emit('newEmail', {
+    //     from: 'mike@example.com',
+    //     text: 'Hey. What is going on.',
+    //     createAt: 123
+    // });
+    //
+    // // we can use an arrow func cuz in our node code.
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+    //
+    // socket.emit('newMessage', {
+    //     from: 'Jack',
+    //     text: 'This message is sent from Server',
+    //     createdAt: new Date().getTime()
+    // });
 
-    // we can use an arrow func cuz in our node code.
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
-    });
-
-    socket.emit('newMessage', {
-        from: 'Jack',
-        text: 'This message is sent from Server',
-        createdAt: new Date().getTime()
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage, new Date().getTime());
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
